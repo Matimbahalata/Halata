2e781a0a3ebdc13c036aac9833376c470826d13b
matimbahalata <tukum.baloyi@gmail.com>
2019-08-05 10:26:38 +0200

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
